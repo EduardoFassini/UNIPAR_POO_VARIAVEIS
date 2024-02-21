8a4159cc84932c99cf7b07979d9b3e44bfd45db4
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-02-20 22:23:15 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,17 @@
+class Atividade2 {
+    
+    public static void main(String[] args) {
+        
+        String nomeProduto
+        double precoProduto
+        String marcaProduto
+        Integer quantidadeProduto
+        String descricaoProduto
+        String nomeProduto
+        Integer numeroProduto
+        double precoProduto
+        Integer estoqueDisponivel
+        Integer totalProdutos
+        
+    }
+}
