e3838a9fe99a7711dcf5a2171a7edf0760742c32
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-02-20 22:06:56 -0300

    Create Atividade1.java

---
+++ Atividade1.java
@@ -0,0 +1,18 @@
+class Atividade1 {
+    
+    
+    public static void main(String[] args) {
+        
+        String nome;
+        Integer idade;
+        char cpf;
+        String rg;
+        String cidade;
+        String endereco;
+        int codigo;
+        float preco;
+        double valor;
+        boolean variave;
+        
+    }
+}
