89973475f4342f2ef8b5f6a5f98c12e0fb4a4f70
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-02-20 22:26:11 -0300

    Update Atividade2.java

--- Atividade2.java
+++ Atividade2.java
@@ -2,16 +2,16 @@ class Atividade2 {
     
     public static void main(String[] args) {
         
-        String nomeProduto
-        double precoProduto
-        String marcaProduto
-        Integer quantidadeProduto
-        String descricaoProduto
-        String nomeProduto
-        Integer numeroProduto
-        double precoProduto
-        Integer estoqueDisponivel
-        Integer totalProdutos
+        String nomeProduto;
+        double precoProduto;
+        String marcaProduto;
+        Integer quantidadeProduto;
+        String descricaoProduto;
+        String nomeProduto2;
+        Integer numeroProduto;
+        double precoProduto2;
+        Integer estoqueDisponivel;
+        Integer totalProdutos;
         
     }
 }
