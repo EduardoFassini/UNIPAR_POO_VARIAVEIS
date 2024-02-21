b6a191096afbb32549089117d74d57aa5233587c
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-02-20 22:34:39 -0300

    Create Atividade3.java

---
+++ Atividade3.java
@@ -0,0 +1,11 @@
+class Atividade3 {
+    
+    public static void main(String[] args) {
+        
+        String nomeAluno;
+        Integer idadeAluno;
+        double notaProva1;
+        double notaProva2;
+        double mediaFinal;
+    }
+}
