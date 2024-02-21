58ff8f1971a35a6c233b315dac0a415c1a087cfe
EduardoFassini <151883534+EduardoFassini@users.noreply.github.com>
2024-02-21 16:42:32 -0300

    Create Atividade6.java

---
+++ Atividade6.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123; 
+        double precoProduto = 25; 
+        
+        String descricaoProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+         
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
